5f8ae28ea84dd51945f61d2a44b2e7f5283ee50d
Shivpratap <shivpratap05oct2007@gmail.com>
2026-05-15 16:05:42 +0530

    frontend setup done

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button";
+
+
+function App() {
+
+
+  return <>
+    <div className="flex items-center justify-center h-screen bg-amber-600">
+      hello world
+      <Button>Hii!!</Button>
+    </div>
+  </>;
+}
+
+export default App;
